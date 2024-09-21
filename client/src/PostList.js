54ab67d951fb48260554aaf2fce153ea347fcb75
bunhok <cbunhok@gmail.com>
2024-09-21 16:23:09 +1200

    Create PostList component

---
+++ client/src/PostList.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const PostList = () => {
+    const [posts, setPosts] = useState({});
+
+    const fetchAllPosts = async () => {
+        const res = await axios.get('http://localhost:4000/posts');
+
+        setPosts(res.data);
+    };
+
+    useEffect(() => {
+        fetchAllPosts();
+    }, []);
+
+    const renderPosts = Object.values(posts).map(post => {
+        return (
+            <div
+                className="card"
+                style={{ width: '30%', marginBottom: '20px' }}
+                key={post.id}
+            >
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <div className="d-flex flex-row flew-wrap justify-content-between">
+            {renderPosts}
+        </div>
+    );
+};
+
+export default PostList;
