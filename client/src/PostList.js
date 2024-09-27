edb9b3316bed05f07516ae2b92c11333075ccc3c
bunhok <cbunhok@gmail.com>
2024-09-27 16:56:57 +1200

    Create CommentCreate component

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import CommentCreate from "./CommentCreate";
 
 const PostList = () => {
     const [posts, setPosts] = useState({});
@@ -22,7 +23,8 @@ const PostList = () => {
                 key={post.id}
             >
                 <div className="card-body">
-                    <h3>{post.title}</h3>
+                    <h3>{post.id} - {post.title}</h3>
+                    <CommentCreate postId={post.id} />
                 </div>
             </div>
         );
